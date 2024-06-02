467775b395d8145e93887fb45931d3996519a343
mshr0969 <mshr60531@gmail.com>
2024-06-02 21:35:21 +0900

    init

---
+++ src/main/kotlin/com/example/kotlin_spring_crud/KotlinSpringCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spring_crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringCrudApplication>(*args)
+}
